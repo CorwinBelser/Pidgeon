a303bed0c2ded8d7aee7fe29b85d4644fb8cff2b
Corwin Belser <cmb539@drexel.edu>
2018-01-31 00:07:20 -0500

    Implemented picking up and dropping objects, including test scene. Move pigeon around manually to trigger pickups. Right click or left shift to drop objects

---
+++ Assets/Decor/Scripts/Collectible.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Collectible : MonoBehaviour {
+
+    public float FOLLOW_VELOCITY = 2.5f;
+    public float MAX_VELOCITY = 15f;
+    public float MAX_FORCE = 40f;
+    public float GAIN = 5f;
+    public float PICKUP_COOLDOWN = 5f;
+    private Vector3 _spawnPosition;
+    private float _respawnMagnitude = 1000f;
+    public float RESPAWN_HEIGHT = 5f;
+    private Transform _target;
+    private float _timeDropped = -5f;
+    private Rigidbody _rigidbody;
+
+    void Start()
+    {
+        _spawnPosition = this.transform.position;
+        _rigidbody = this.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (_target != null)
+        {
+            /* Taken from https://answers.unity.com/answers/195790/view.html */
+            Vector3 distance = _target.position - this.transform.position;
+            Vector3 targetVelocity = Vector3.ClampMagnitude(FOLLOW_VELOCITY * distance, MAX_VELOCITY);
+            Vector3 error = targetVelocity - _rigidbody.velocity;
+            Vector3 force = Vector3.ClampMagnitude(GAIN * error, MAX_FORCE);
+            
+            /* Apply the force */
+            _rigidbody.AddForce(force);
+        }
+        else if (this.transform.position.sqrMagnitude > _respawnMagnitude)
+        {
+            /* Respawn the object, as it is likely out of bounds */
+            this.transform.position = new Vector3(_spawnPosition.x, _spawnPosition.y + RESPAWN_HEIGHT, _spawnPosition.z);
+            /* Clear out any existing forces */
+            _rigidbody.velocity = Vector3.zero;
+            _rigidbody.angularVelocity = Vector3.zero;
+        }
+    }
+
+    public bool Pickup(Transform target)
+    {
+        if (Time.time - _timeDropped > PICKUP_COOLDOWN)
+        {
+            Debug.Log("<color=blue>    (Collectible): " + this.name + " now following " + target.name + "</color>");
+            _target = target;
+            return true;
+        }
+        //Debug.Log("<color=green>    (Collectible): Collectible is on cooldown and can't be picked up</color");
+        return false;
+    }
+
+    public void Drop()
+    {
+        _timeDropped = Time.time;
+        _target = null;
+    }
+}
---
+++ Assets/Pidgeon/Scripts/PigeonGrab.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigeonGrab : MonoBehaviour {
+    private static string COLLECTIBLE_TAG = "Collectible";
+    private List<Transform> _heldObjects;
+
+    void Start()
+    {
+        _heldObjects = new List<Transform>();
+    }
+
+    void Update()
+    {
+        /* If the player presses Fire2, drop every item */
+        if (Input.GetButtonDown("Fire2") && _heldObjects.Count != 0)
+        {
+            for (int i = _heldObjects.Count - 1; i >= 0; i--)
+            {
+                //Debug.Log("<color=blue>(Pigeon): Dropping " + _heldObjects[i].name + " </color>");
+                _heldObjects[i].GetComponent<Collectible>().Drop();
+                _heldObjects.RemoveAt(i);
+            }
+        }
+    }
+    
+    void OnTriggerEnter(Collider coll)
+    {
+        //Debug.Log("<color=green>(Pigeon): Trying to pickup " + coll.name + "</color>");
+        /* Check if the thing below is a collectible */
+        if (coll.tag == COLLECTIBLE_TAG && !_heldObjects.Contains(coll.transform))
+        {
+            //Debug.Log("<color=green>    Tag matched! Picking up...</Color>");
+            /* Tell the collectible to follow the last grabbed collectible */
+            bool succeeded = false;
+            if (_heldObjects.Count != 0)
+                succeeded = coll.gameObject.GetComponent<Collectible>().Pickup(_heldObjects[_heldObjects.Count - 1]);
+            else
+                succeeded = coll.gameObject.GetComponent<Collectible>().Pickup(this.transform);
+            
+            /* Add the collectible to the list of held items */
+            if (succeeded)
+                _heldObjects.Add(coll.transform);
+        }
+        else
+        {
+            //Debug.Log("<Color=green>    Tag didn't match or already held</color>");
+        }
+    }
+}
