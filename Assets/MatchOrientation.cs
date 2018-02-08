0acad7ef2fd6994dab33057dbc25e0093b5de95a
James Swanick <swanijam@gmail.com>
2018-02-08 00:01:08 -0500

    Shitty Ragdoll

---
+++ Assets/MatchOrientation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchOrientation : MonoBehaviour {
+	public float Scale = .3f;
+	public Transform target;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localRotation = Quaternion.Slerp(transform.localRotation, target.localRotation, Scale);
+	}
+}
---
+++ Assets/ViewSkeleton.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ViewSkeleton : MonoBehaviour 
+{
+
+	public Transform rootNode;
+	public Transform[] childNodes;
+
+	void OnDrawGizmos()
+	{
+		if (rootNode != null)
+		{
+			if(childNodes == null || childNodes.Length == 0)
+			{
+				//get all joints to draw
+				PopulateChildren();
+			}
+
+
+			foreach (Transform child in childNodes)
+			{
+
+				if (child == rootNode)
+				{
+					//list includes the root, if root then larger, green cube
+					Gizmos.color = Color.green * 2f;
+					Gizmos.DrawCube(child.position, new Vector3(.06f, .06f, .06f));
+				}
+				else
+				{
+					Gizmos.color = Color.white * 4f;
+					Gizmos.DrawLine(child.position, child.parent.position);
+					Gizmos.color = Color.cyan * 2f;
+					Gizmos.DrawCube(child.position, new Vector3(.05f, .05f, .05f));
+					Gizmos.color = Color.red * 2f;
+					Gizmos.DrawRay (transform.position, transform.forward);
+				}
+			}
+
+		}
+	}
+
+	public void PopulateChildren()
+	{
+		childNodes = rootNode.GetComponentsInChildren<Transform>();
+	}
+}
