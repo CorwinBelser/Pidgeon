c8553a58fb04b8ca30cb03de5d3673b0baf5bc14
CorwinBelser <cmb539@drexel.edu>
2018-02-25 02:00:48 -0500

    Added hoops for pigeon to fly through. Flying through a hoop hides the hoop, makes the next hoop bold, and the one after transparent

---
+++ Assets/Hoop/Scripts/IReceiveTriggerFromChildren.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IReceiveTriggerFromChildren {
+
+	void OnChildTriggerEnter(Collider col);
+	void OnChildTriggerExit(Collider col);
+	void OnChildTriggerStay(Collider col);
+}
