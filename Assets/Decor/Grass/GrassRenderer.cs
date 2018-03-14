1f6ffba49d04d865a8eb11b2c9a8a82221ffc0e1
CorwinBelser <cmb539@drexel.edu>
2018-03-14 19:14:21 -0400

    Grass is now only spawned once, and stored. Reduced the amount of grass generators in FinalScene.

--- Assets/Decor/Grass/GrassRenderer.cs
+++ Assets/Decor/Grass/GrassRenderer.cs
@@ -13,34 +13,33 @@ public class GrassRenderer : MonoBehaviour {
     public int numGrasses;
     public int seed;
 
-    public float startHeight = 1000;
+    public float radius = 1000f;
     public float GrassOffSet = 0f;
 	public LayerMask mask;
+    private List<Matrix4x4> _matrices;
 
     // Use this for initialization
     void Start () {
-		
+		Random.InitState(seed);
+        _matrices = new List<Matrix4x4>(numGrasses);
+        for(int i = 0; i < numGrasses; i++)
+        {
+            Vector3 origin = new Vector3(
+                this.transform.position.x + size.x * Random.Range(-.5f, .5f),
+                this.transform.position.y,
+                transform.position.z + size.y * Random.Range(-.5f, .5f));
+
+            Ray ray = new Ray(origin, Vector3.down);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, radius, mask))
+            {
+                _matrices.Add(Matrix4x4.TRS(hit.point + (Vector3.up * GrassOffSet), Quaternion.identity, Vector3.one));
+            }
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Random.InitState(seed);
-        List<Matrix4x4> matrices = new List<Matrix4x4>(numGrasses);
-        for(int i = 0; i < numGrasses; i++)
-        {
-            Vector3 origin = transform.position;
-            origin.y = startHeight;
-            origin.x += size.x * Random.Range(-.5f, .5f);
-            origin.z += size.y * Random.Range(-.5f, .5f);
-            Ray ray = new Ray(origin, Vector3.down);
-            RaycastHit hit;
-            if(Physics.Raycast(ray, out hit,1000f,mask))
-            {
-                //origin = hit.point;
-                origin = hit.point + (Vector3.up * GrassOffSet);
-                matrices.Add(Matrix4x4.TRS(origin, Quaternion.identity, Vector3.one /*/* Random.Range(.5f, 5))*/));
-            }
-        }
-        Graphics.DrawMeshInstanced(grassMesh,0, grassMaterial, matrices, null, UnityEngine.Rendering.ShadowCastingMode.Off);
+        Graphics.DrawMeshInstanced(grassMesh,0, grassMaterial, _matrices, null, UnityEngine.Rendering.ShadowCastingMode.Off);
 	}
 }
